df538cb19e0f9eb65d2c8445c2ba378696ec9dfb
Tran Lam <lamtranchinghi@gmail.com>
2025-03-07 14:51:06 -0600

    Implemented REPL

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,11 @@ int main() {
   std::cerr << std::unitbuf;
 
   // Uncomment this block to pass the first stage
-  std::cout << "$ ";
-
-  std::string input;
-  std::getline(std::cin, input);
-  std::cout << input << ": command not found\n";
+  
+  while (true) {
+    std::string input;
+    std::cout << "$ ";
+    std::getline(std::cin, input);
+    std::cout << input << ": command not found\n";
+  }
 }
