d4cb8c71138544b5917fe95182618fd44c28fcf8
Tran Lam <lamtranchinghi@gmail.com>
2025-03-07 15:35:47 -0600

    implemented echo builtin

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@ int main() {
     std::cout << "$ ";
     std::getline(std::cin, input);
     if (input == "exit 0") return 0;
+    else if (input.find("echo") != std::string::npos) std::cout << input.substr(5) << "\n";
     else std::cout << input << ": command not found\n";
   }
   return 0; 
