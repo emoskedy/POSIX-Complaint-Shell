41e1b1d5d9ba9865016a77d1b5a9b98213daaf22
Tran Lam <lamtranchinghi@gmail.com>
2025-03-08 23:05:23 -0600

    implemented type builtin for executable files

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,29 @@
 #include <iostream>
+#include <vector>
+#include <set>
+#include <string>
+#include <sstream>
+#include <filesystem>
+namespace fs = std::filesystem;
 
 int main() {
   // Flush after every std::cout / std:cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
-
+  char* path = getenv("PATH");
+  std::stringstream ss(path);
+  std::string dir;
+  std::vector<std::string> tokens;
+  while (std::getline(ss, dir, ':')) {
+    tokens.push_back(dir);
+  }
+  
   while (true) {
-    std::string input;
     std::cout << "$ ";
+    
+    std::string input;
     std::getline(std::cin, input);
+    
     if (input.find("exit") == 0) {
       break;
     } 
@@ -16,14 +31,29 @@ int main() {
       std::cout << input.substr(5) << "\n";
     }
     else if (input.find("type") == 0) {
-      if (input.substr(5) == "echo" || input.substr(5) == "exit" || input.substr(5) == "type") {
-        std::cout << input.substr(5) << " is a shell builtin\n";
+      std::string commandType = input.substr(5);
+      std::set<std::string> st = {"echo", "exit", "type"};
+      if (st.find(commandType) != st.end()) {
+        std::cout << commandType << " is a shell builtin\n";
       }
       else {
-        std::cout << input.substr(5) << ": not found\n";
+        bool notFound = false;
+        for (auto dir : tokens) {
+          for (auto entry : fs::directory_iterator(dir)) {
+            if (commandType == entry.path().filename()) {
+              notFound = true;
+              std::cout << commandType << " is " << entry.path().string() << "\n";
+            }
+          }
+          if (notFound) break;
+        }
+        if (!notFound) std::cout << commandType << ": not found\n";
       }
     }
-    else std::cout << input << ": command not found\n";
+    else {
+      std::cout << input << ": command not found\n";
+    }
+    // for (std::string a : tokens) std::cout << a << "\n";
   }
   return 0; 
 }
